b94781224f23d4a085c20a188af5bb7c08d52238
Matas Siaurodinas <matas.siaurodinas@gmail.com>
2025-11-02 22:51:06 +0200

    Stair jump fix

--- Assets/Scripts/Player/playerStairStep.cs
+++ Assets/Scripts/Player/playerStairStep.cs
@@ -33,6 +33,19 @@ public class PlayerStairStepSystem
         // Cast ray only in the main movement direction
         if (Physics.Raycast(origin, mainDir, out hit, rayDistance) && hit.collider.CompareTag("Stairs"))
         {
+            if (_pc.playerInputHandler.JumpTriggered)
+            {
+                _pc.Animator.animator.SetTrigger("Jump");
+                _pc.Animator.animator.SetBool("isRunning", false);
+                _pc.Animator.animator.SetBool("isWalking", false);
+                _pc.rb.linearVelocity = new Vector3(
+                    _pc.rb.linearVelocity.x,
+                    _pc.jumpForce,
+                    _pc.rb.linearVelocity.z
+                );
+                return;
+            }
+            
             if(_pc.playerInputHandler.SprintTriggered && !_pc.playerInputHandler.CrouchTriggered)
             {
                 _pc.Animator.animator.SetBool("isRunning", true);
@@ -55,6 +68,19 @@ public class PlayerStairStepSystem
             Vector3 diagonalDir = _pc.playerSkin.TransformDirection(new Vector3(movementInput.x, 0f, movementInput.y).normalized);
             if (Physics.Raycast(origin, diagonalDir, out hit, rayDistance) && hit.collider.CompareTag("Stairs"))
             {
+                if (_pc.playerInputHandler.JumpTriggered)
+                {
+                    _pc.Animator.animator.SetTrigger("Jump");
+                    _pc.Animator.animator.SetBool("isRunning", false);
+                    _pc.Animator.animator.SetBool("isWalking", false);
+                    _pc.rb.linearVelocity = new Vector3(
+                        _pc.rb.linearVelocity.x,
+                        _pc.jumpForce,
+                        _pc.rb.linearVelocity.z
+                    );
+                    return;
+                }
+
                 if(_pc.playerInputHandler.SprintTriggered && !_pc.playerInputHandler.CrouchTriggered)
                 {
                     _pc.Animator.animator.SetBool("isRunning", true);
